ad870a375477c1294bde46cd47811c9ef7da7834
Patricio Vidal <PatricioVidal@users.noreply.github.com>
2008-05-07 04:04:06 +0000

    - Cambiado a versión 1.28.

--- ManejadorDeMapa/CommonAssemblyInfo.cs
+++ ManejadorDeMapa/CommonAssemblyInfo.cs
@@ -89,6 +89,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.29.*")]
+[assembly: AssemblyVersion("1.28.*")]
 
 [assembly: NeutralResourcesLanguage("es-VE")]
